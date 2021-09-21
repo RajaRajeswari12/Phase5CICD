a322da336f34d48591b5b68cb3397971f1e293ff
Raja Rajeswari <talktokraji@gmail.com>
2021-09-21 18:48:32 +0530

    Modified Test Data

--- src/test/java/com/example/springSimplilearn/springDevOps/SpringDevOpsApplicationTests.java
+++ src/test/java/com/example/springSimplilearn/springDevOps/SpringDevOpsApplicationTests.java
@@ -34,7 +34,7 @@ class SpringDevOpsApplicationTests {
 
 	    @Test
 	    public void addsNewEmployee() throws Exception {
-	        String newEmployee = "{\"empI\":10,\"name\":\"Raji\",\"city\":\"Bangalore\" }";
+	        String newEmployee = "{\"empId\":12,\"name\":\"Raji\",\"city\":\"Bangalore\" }";
 	        mockMvc.perform(MockMvcRequestBuilders.post("/employee")
 	                .contentType(MediaType.APPLICATION_JSON)
 	                .content(newEmployee)
